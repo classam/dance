865383acda407321f69728b9f0804fae1477f1c4
Curtis Lassam <curtis@vrchat.net>
2016-10-31 00:45:30 -0700

    black screen

--- renderer.js
+++ renderer.js
@@ -1,3 +1,17 @@
 // This file is required by the index.html file and will
 // be executed in the renderer process for that window.
 // All of the Node.js APIs are available in this process.
+
+const PIXI = require('pixi.js');
+
+//Create the renderer
+var renderer = PIXI.autoDetectRenderer(800, 800);
+
+//Add the canvas to the HTML document
+document.body.appendChild(renderer.view);
+
+//Create a container object called the `stage`
+var stage = new PIXI.Container();
+
+//Tell the `renderer` to `render` the `stage`
+renderer.render(stage);
